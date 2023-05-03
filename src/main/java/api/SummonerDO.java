62d7fdb1147eae71ba46871eb1e574eedc8b7686
Lee Seung Ho <plok7173@gmail.com>
2023-05-03 15:55:03 +0900

    Revert "Revert "Riot API 작업 현황 커밋""
    This reverts commit 57c6e08d95c6c7491ede015efec51d4982f2bc37.

---
+++ src/main/java/api/SummonerDO.java
@@ -0,0 +1,50 @@
+package api;
+
+public class SummonerDO {
+	private String accountId;
+	private String id;
+	private String name;
+	private int summonerLevel;
+	private String puuid;
+
+	public String getPuuid() {
+		return puuid;
+	}
+
+	public void setPuuid(String puuid) {
+		this.puuid = puuid;
+	}
+
+	public String getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSummonerLevel() {
+		return summonerLevel;
+	}
+
+	public void setSummonerLevel(int summonerLevel) {
+		this.summonerLevel = summonerLevel;
+	}
+
+}
