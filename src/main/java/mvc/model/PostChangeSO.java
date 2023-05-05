fd7683141ff721cce3887f0de8667fa941d96bae
ppssh <p303sh@gmail.com>
2023-05-05 10:15:14 +0900

    PostChangeSO 수정본 (PostUpdateCommand 필요)

--- src/main/java/mvc/model/PostChangeSO.java
+++ src/main/java/mvc/model/PostChangeSO.java
@@ -1,49 +1,69 @@
 package mvc.model;
 
+import java.util.Date;
+
 public class PostChangeSO {
-    private PostDAO postDao;
+    private PostDao postDao;
 
     public PostChangeSO() {
-        postDao = new PostDAO(dataSource);
+        this.postDao = new PostDao();
     }
 
-    public PostChangeSO(PostDAO postDao) {
+    public PostChangeSO(PostDao postDao) {
         this.postDao = postDao;
     }
 
-    public boolean update(PostUpdateCommand command) {
-        int postId = command.getPostId();
-        PostDO existingPost = postDao.selectById(postId);
-
-        if (existingPost == null) {
-            return false;
+    public void update(PostUpdateCommand command) {
+        PostDO postDO = postDao.search(command.getPostId());
+        if (postDO != null) {
+            if (command.getCategory() != null) {
+                postDO.setCategory(command.getCategory());
+            }
+            if (command.getTitle() != null) {
+                postDO.setTitle(command.getTitle());
+            }
+            if (command.getContent() != null) {
+                postDO.setContent(command.getContent());
+            }
+            postDO.setPost_date(new Date());
+            postDao.update(postDO);
+            System.out.println("게시물이 수정되었습니다.");
+        } else {
+            System.out.println("해당 게시물이 존재하지 않습니다.");
         }
-
-        String newTitle = command.getTitle();
-        String newContent = command.getContent();
-
-        if (newTitle != null && !newTitle.equals(existingPost.getTitle())) {
-            existingPost.changeTitle(existingPost.getTitle(), newTitle);
-        }
-
-        if (newContent != null && !newContent.equals(existingPost.getContent())) {
-            existingPost.changeContent(existingPost.getContent(), newContent);
-        }
-
-        if (existingPost.getTitle().equals(command.getTitle()) && existingPost.getContent().equals(command.getContent())) {
-            return false;
-        }
-
-        return postDao.postUpdateAll(existingPost);
     }
 
-    public boolean delete(int postId) {
-        PostDO existingPost = postDao.selectById(postId);
-
-        if (existingPost == null) {
-            return false;
+    public void delete(int postId) {
+        PostDO postDO = postDao.search(postId);
+        if (postDO != null) {
+            postDao.delete(postDO);
+            System.out.println("게시물이 삭제되었습니다.");
+        } else {
+            System.out.println("해당 게시물이 존재하지 않습니다.");
         }
+    }
 
-        return postDao.postDeleteById(postId);
+    public void recommend(int postId) {
+        postDao.recommend(postId);
+    }
+
+    public void unrecommend(int postId) {
+        postDao.unrecommend(postId);
+    }
+
+    public void report(int postId) {
+        postDao.report(postId);
+    }
+
+    public void notice(int postId) {
+        postDao.notice(postId);
+    }
+
+    public PostDao getPostDao() {
+        return postDao;
+    }
+
+    public void setPostDao(PostDao postDao) {
+        this.postDao = postDao;
     }
 }
